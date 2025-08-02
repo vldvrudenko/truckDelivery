0abbf2b41bc9c8ded347fb5ac208fbab7210e9d6
Volodymyr Rudenko <lans@MacBook-Air-Volodymyr.local>
2025-08-02 06:23:51 -0600

    01.08 truck_delivery 2.0

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+window.addEventListener("DOMContentLoaded", () => {
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 10,
+
+  breakpoints: {
+    400: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+})
 const navbar = document.querySelector(".navbar");
 navbar.querySelector(".toggle").addEventListener("click", () => {
   navbar.classList.toggle("collapsed");
