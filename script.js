b5733d20ba582a0b6ad6ae36daf2599cc5fa7cee
Volodymyr Rudenko <lans@MacBook-Air-Volodymyr.local>
2025-08-11 00:29:36 -0600

    08.08 truck_delviery 2.0 - contact us page

--- script.js
+++ script.js
@@ -138,3 +138,15 @@ scrollTopBtn.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+document.querySelectorAll('.scroll-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    
+    window.scrollTo({
+      top: targetElement.offsetTop - 50,
+      behavior: 'smooth'
+    });
+  });
+});
